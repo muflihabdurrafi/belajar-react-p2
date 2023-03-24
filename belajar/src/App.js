5a8d8036381a0c102160fd3c0e420e1aa0fde60c
Muflih <69343587+mufmufmuf@users.noreply.github.com>
2023-03-24 21:31:13 +0700

    coba2 routing gagal

--- belajar/src/App.js
+++ belajar/src/App.js
@@ -1,18 +1,20 @@
-import logo from "./logo.svg";
 import "./App.css";
+import { Routes, Route, Link } from "react-router-dom";
+import Home from "./Home";
+import About from "./About";
 
 function App() {
     return (
         <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-                    Learn React
-                </a>
-            </header>
+            <h1>gajelas</h1>
+            {/* <nav>
+                <Link to="/">Home</Link>
+                <Link to="/about">About</Link>
+            </nav> */}
+            {/* <Routes>
+                <Route path="/" element={<Home />}></Route>
+                <Route path="about" element={<About />}></Route>
+            </Routes> */}
         </div>
     );
 }
